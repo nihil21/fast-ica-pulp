f2ff6f7af2ce6da237fa741dd6a44bea0d2fdbe1
Mattia Orlandi <mattia.orlandi21@gmail.com>
2021-12-06 14:43:53 +0100

    Modified benchmark

--- workspace/fast-ica/run_benchmark.sh
+++ workspace/fast-ica/run_benchmark.sh
@@ -4,9 +4,9 @@
 for s in 0 1; do
     # Iterate over G functions
     for g in 0 1 2; do
-        echo "Strategy: $s, G: $g";
-        make clean all run STRATEGY=$s G_FUNC=$g VERB=1 >> report_$s\_$g.log
-        echo "Done";
+        echo "Strategy: $s, G: $g" | tee -a report_strategy.txt;
+        make clean all run STRATEGY=$s G_FUNC=$g VERB=1 | tee -a report_strategy.txt
+        echo "Done" | tee -a report_strategy.txt;
         sleep 10;
     done
 done
